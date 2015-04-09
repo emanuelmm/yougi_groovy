72a4655f9f33551ebec3c773d1cf7a8da01f833b
eprogramming <00hf11@gmail.com>
2015-04-09 19:49:08 -0300

    updates

--- src/main/groovy/org/yougi/web/model/UserSessionMBean.groovy
+++ src/main/groovy/org/yougi/web/model/UserSessionMBean.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,32 +18,27 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.web.model;
+package org.yougi.web.model
 
-import org.yougi.business.UserSessionBean;
-import org.yougi.entity.UserSession;
+import org.yougi.business.UserSessionBean
+import org.yougi.entity.UserSession
 
-import javax.ejb.EJB;
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Named;
-import java.util.List;
+import javax.ejb.EJB
+import javax.enterprise.context.RequestScoped
+import javax.inject.Named
 
 /**
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
 @Named
 @RequestScoped
-public class UserSessionMBean {
+class UserSessionMBean {
 
-    @EJB
-    private UserSessionBean userSessionBean;
+  @EJB
+  private UserSessionBean userSessionBean
+  def userSessions
 
-    private List<UserSession> userSessions;
-
-    public List<UserSession> getUserSessions() {
-        if(this.userSessions == null) {
-            this.userSessions = userSessionBean.findActiveSessions();
-        }
-        return this.userSessions;
-    }
+  def getUserSessions() {
+    userSessions ?: userSessionBean.findActiveSessions()
+  }
 }
