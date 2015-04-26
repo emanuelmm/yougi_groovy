27dc6f5dca3906ceafd581a41508b8b0961c4a66
eprogramming <00hf11@gmail.com>
2015-04-25 22:25:16 -0300

    #27

--- src/main/groovy/org/yougi/entity/City.groovy
+++ src/main/groovy/org/yougi/entity/City.groovy
@@ -1,27 +1,27 @@
-/* Yougi is a web application conceived to manage user groups or 
- * communities focused on a certain domain of knowledge, whose members are 
- * constantly sharing information and participating in social and educational 
+/* Yougi is a web application conceived to manage user groups or
+ * communities focused on a certain domain of knowledge, whose members are
+ * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
- * 
- * This application is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by the 
- * Free Software Foundation; either version 2.1 of the License, or (at your 
+ *
+ * This application is free software you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
- * 
- * This application is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ *
+ * This application is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
- * 
- * There is a full copy of the GNU Lesser General Public License along with 
+ *
+ * There is a full copy of the GNU Lesser General Public License along with
  * this library. Look for the file license.txt at the root level. If you do not
- * find it, write to the Free Software Foundation, Inc., 59 Temple Place, 
+ * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.entity;
+package org.yougi.entity
 
-import javax.persistence.*;
-import java.io.Serializable;
+import javax.persistence.*
+import java.io.Serializable
 
 /**
  * City is the smallest geographic region where a JUG can operate. A JUG can
@@ -33,135 +33,30 @@ import java.io.Serializable;
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
 @Entity
-@Table(name = "city")
-public class City implements Serializable, Identified {
+@Table(name = 'city')
+class City implements Serializable, Identified {
 
-    private static final long serialVersionUID = 1L;
+  @Id
+  String id
+  String name
+  Boolean valid
 
-    @Id
-    private String id;
+  @JoinColumn(name = 'country')
+  @ManyToOne(optional = false)
+  Country country
 
-    private String name;
+  @ManyToOne
+  @JoinColumn(name = 'province')
+  Province province
 
-    private Boolean valid;
+  String latitude
+  String longitude
 
-    @JoinColumn(name = "country")
-    @ManyToOne(optional = false)
-    private Country country;
+  @Column(name='timezone')
+  String timeZone
 
-    @ManyToOne
-    @JoinColumn(name = "province")
-    private Province province;
-
-    private String latitude;
-
-    private String longitude;
-
-    @Column(name="timezone")
-    private String timeZone;
-
-    public City() {
-    }
-
-    public City(String id) {
-        this.id = id;
-    }
-
-    public City(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    @Override
-    public String getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Boolean getValid() {
-        return valid;
-    }
-
-    public void setValid(Boolean valid) {
-        this.valid = valid;
-    }
-
-    public Country getCountry() {
-        return country;
-    }
-
-    public void setCountry(Country country) {
-        this.country = country;
-    }
-
-    public Province getProvince() {
-        return province;
-    }
-
-    public void setProvince(Province province) {
-        this.province = province;
-    }
-
-    public String getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(String latitude) {
-        this.latitude = latitude;
-    }
-
-    public String getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(String longitude) {
-        this.longitude = longitude;
-    }
-
-    /**
-     * @return the timezone of the city.
-     */
-    public String getTimeZone() {
-        return timeZone;
-    }
-
-    public void setTimeZone(String timeZone) {
-        this.timeZone = timeZone;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (!(object instanceof City)) {
-            return false;
-        }
-        City other = (City) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
+  @Override
+  String toString() {
+    name
+  }
 }
