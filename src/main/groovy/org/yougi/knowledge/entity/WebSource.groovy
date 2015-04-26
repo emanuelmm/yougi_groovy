538f3d6335130f5fdd806b5f6735458a28856080
eprogramming <00hf11@gmail.com>
2015-04-26 00:51:28 -0300

    #27

--- src/main/groovy/org/yougi/knowledge/entity/WebSource.groovy
+++ src/main/groovy/org/yougi/knowledge/entity/WebSource.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,15 +18,13 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.knowledge.entity;
+package org.yougi.knowledge.entity
 
-import org.yougi.entity.Identified;
-import org.yougi.entity.UserAccount;
+import org.yougi.entity.Identified
+import org.yougi.entity.UserAccount
 
-import javax.persistence.*;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import javax.persistence.*
+import java.io.Serializable
 
 /**
  * Entity class representing an external website used as a source of content for
@@ -35,110 +33,36 @@ import java.util.List;
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
 @Entity
-@Table(name = "web_source")
-public class WebSource implements Serializable, Identified {
+@Table(name = 'web_source')
+class WebSource implements Serializable, Identified {
 
-    private static final long serialVersionUID = 1L;
+  @Id
+  String id
+  String title
+  String feed
 
-    @Id
-    private String id;
-    private String title;
-    private String feed;
+  @ManyToOne
+  @JoinColumn(name='provider')
+  UserAccount provider
 
-    @ManyToOne
-    @JoinColumn(name="provider")
-    private UserAccount provider;
+  @Transient
+  List<Article> articles
 
-    @Transient
-    private List<Article> articles;
-
-    public WebSource() {}
-
-    public WebSource(String id) {
-        this.id = id;
+  void addArticle(Article article) {
+    if(articles == null) {
+      articles = new ArrayList<>()
     }
+    articles.add(article)
+  }
 
-    public WebSource(String title, String feed) {
-        this.title = title;
-        this.feed = feed;
+  void removeArticle(Article article) {
+    if(article && articles) {
+      articles.remove(article)
     }
+  }
 
-    @Override
-    public String getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getFeed() {
-        return feed;
-    }
-
-    public void setFeed(String feed) {
-        this.feed = feed;
-    }
-
-    public UserAccount getProvider() {
-        return provider;
-    }
-
-    public void setProvider(UserAccount provider) {
-        this.provider = provider;
-    }
-
-    public List<Article> getArticles() {
-        return articles;
-    }
-
-    public void setArticles(List<Article> articles) {
-        this.articles = articles;
-    }
-
-    public void addArticle(Article article) {
-        if(this.articles == null) {
-            this.articles = new ArrayList<>();
-        }
-        this.articles.add(article);
-    }
-
-    public void removeArticle(Article article) {
-        if(article != null && this.articles != null) {
-            this.articles.remove(article);
-        }
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (!(object instanceof WebSource)) {
-            return false;
-        }
-        WebSource other = (WebSource) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return this.title;
-    }
+  @Override
+  String toString() {
+    title
+  }
 }
