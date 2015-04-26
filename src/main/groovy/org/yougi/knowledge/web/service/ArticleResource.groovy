c0153e8488b28b63edcefb98764116b5f6412d75
eprogramming <00hf11@gmail.com>
2015-04-26 02:11:45 -0300

    #27

--- src/main/groovy/org/yougi/knowledge/web/service/ArticleResource.groovy
+++ src/main/groovy/org/yougi/knowledge/web/service/ArticleResource.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,30 +18,30 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.knowledge.web.service;
+package org.yougi.knowledge.web.service
 
-import org.yougi.knowledge.business.ArticleBean;
-import org.yougi.knowledge.entity.Article;
+import org.yougi.knowledge.business.ArticleBean
+import org.yougi.knowledge.entity.Article
 
-import javax.annotation.ManagedBean;
-import javax.ejb.EJB;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.annotation.ManagedBean
+import javax.ejb.EJB
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
 
 /**
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
-@Path("articles")
+@Path('articles')
 @ManagedBean
-public class ArticleResource {
+class ArticleResource {
 
-    @EJB
-    private ArticleBean articleBean;
+  @EJB
+  private ArticleBean articleBean
 
-    @GET
-    @Produces("application/xml")
-    public List<Article> getAll() {
-        return articleBean.findPublishedArticles();
-    }
+  @GET
+  @Produces('application/xml')
+  List<Article> getAll() {
+    articleBean.findPublishedArticles()
+  }
 }
