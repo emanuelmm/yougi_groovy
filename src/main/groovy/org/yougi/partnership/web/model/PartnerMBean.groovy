ab6c1399a326a488f70970451f357006d3455df0
eprogramming <00hf11@gmail.com>
2015-04-26 02:09:10 -0300

    #27

--- src/main/groovy/org/yougi/partnership/web/model/PartnerMBean.groovy
+++ src/main/groovy/org/yougi/partnership/web/model/PartnerMBean.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,172 +18,126 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.partnership.web.model;
+package org.yougi.partnership.web.model
 
-import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.inject.Named;
+import javax.annotation.PostConstruct
+import javax.ejb.EJB
+import javax.enterprise.context.RequestScoped
+import javax.inject.Inject
+import javax.inject.Named
 
-import org.primefaces.model.DualListModel;
-import org.yougi.annotation.ManagedProperty;
-import org.yougi.business.AccessGroupBean;
-import org.yougi.business.UserGroupBean;
-import org.yougi.entity.AccessGroup;
-import org.yougi.entity.Address;
-import org.yougi.entity.City;
-import org.yougi.entity.Country;
-import org.yougi.entity.Province;
-import org.yougi.entity.UserAccount;
-import org.yougi.partnership.business.PartnerBean;
-import org.yougi.partnership.business.RepresentativeBean;
-import org.yougi.partnership.entity.Partner;
-import org.yougi.partnership.entity.Representative;
-import org.yougi.web.model.LocationMBean;
+import org.primefaces.model.DualListModel
+import org.yougi.annotation.ManagedProperty
+import org.yougi.business.AccessGroupBean
+import org.yougi.business.UserGroupBean
+import org.yougi.entity.AccessGroup
+import org.yougi.entity.Address
+import org.yougi.entity.City
+import org.yougi.entity.Country
+import org.yougi.entity.Province
+import org.yougi.entity.UserAccount
+import org.yougi.partnership.business.PartnerBean
+import org.yougi.partnership.business.RepresentativeBean
+import org.yougi.partnership.entity.Partner
+import org.yougi.partnership.entity.Representative
+import org.yougi.web.model.LocationMBean
 
 /**
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
 @Named
 @RequestScoped
-public class PartnerMBean implements Serializable {
+class PartnerMBean implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+  @EJB
+  private PartnerBean partnerBean
+  @EJB
+  private AccessGroupBean accessGroupBean
+  @EJB
+  private UserGroupBean userGroupBean
+  @EJB
+  private RepresentativeBean representativeBean
 
-    @EJB
-    private PartnerBean partnerBean;
+  @Inject
+  @ManagedProperty('#{param.id}')
+  String id
 
-    @EJB
-    private AccessGroupBean accessGroupBean;
+  @Inject
+  private LocationMBean locationMBean
 
-    @EJB
-    private UserGroupBean userGroupBean;
+  Partner partner
+  private List<Partner> partners
+  List<Representative> representatives
 
-    @EJB
-    private RepresentativeBean representativeBean;
+  // List of users from the group of partners, which are candidates for
+  // representative.
+  DualListModel<UserAccount> candidates
 
-    @Inject
-    @ManagedProperty("#{param.id}")
-    private String id;
+  List<Partner> getPartners() {
+    if (partners == null) {
+      partners = partnerBean.findPartners()
+    }
+    partners
+  }
 
-    @Inject
-    private LocationMBean locationMBean;
+  @PostConstruct
+  void load() {
+    List<UserAccount> reps = new ArrayList<>()
+    AccessGroup accessGroup = accessGroupBean.findAccessGroupByName('partners')
+    List<UserAccount> usersGroup = userGroupBean.findUsersGroup(accessGroup)
 
-    private Partner partner;
-    private List<Partner> partners;
-    private List<Representative> representatives;
+    if (id) {
+      partner = partnerBean.find(id)
+      locationMBean.initialize()
 
-    // List of users from the group of partners, which are candidates for
-    // representative.
-    private DualListModel<UserAccount> candidates;
+      if (partner.address.country) {
+        locationMBean.setSelectedCountry(partner.address.country.acronym)
+      }
 
-    public PartnerMBean() {
+      if (partner.address.province) {
+        locationMBean.setSelectedProvince(partner.address.province.id)
+      }
+
+      if (partner.address.city) {
+        locationMBean.setSelectedCity(partner.address.city.id)
+      }
+
+      reps.addAll(representativeBean.findRepresentativePersons(partner))
+      usersGroup.removeAll(reps)
+    } else {
+      partner = new Partner().setAddress(new Address())
+    }
+    candidates = new DualListModel<>(usersGroup, reps)
+  }
+
+  String save() {
+    Country country = locationMBean.getCountry()
+    if (country) {
+      partner.address.setCountry(country)
     }
 
-    public String getId() {
-        return id;
+    Province province = this.locationMBean.getProvince()
+    if (province != null) {
+      this.partner.getAddress().setProvince(province)
     }
 
-    public void setId(String id) {
-        this.id = id;
+    City city = this.locationMBean.getCity()
+    if (city != null) {
+      this.partner.getAddress().setCity(city)
     }
 
-    public Partner getPartner() {
-        return partner;
+    List<UserAccount> reps = new ArrayList<>()
+    List<UserAccount> selectedCandidates = this.candidates.getTarget()
+    for (UserAccount selectedCandidate : selectedCandidates) {
+      reps.add(new UserAccount(selectedCandidate.getId()))
     }
 
-    public void setPartner(Partner partner) {
-        this.partner = partner;
-    }
+    representativeBean.save(this.partner, reps)
+    'partners?faces-redirect=true'
+  }
 
-    public List<Partner> getPartners() {
-        if (partners == null) {
-            this.partners = partnerBean.findPartners();
-        }
-        return partners;
-    }
-
-    public List<Representative> getRepresentatives() {
-        return representatives;
-    }
-
-    public LocationMBean getLocationMBean() {
-        return locationMBean;
-    }
-
-    public void setLocationMBean(LocationMBean locationMBean) {
-        this.locationMBean = locationMBean;
-    }
-
-    public DualListModel<UserAccount> getCandidates() {
-        return candidates;
-    }
-
-    public void setCandidates(DualListModel<UserAccount> candidates) {
-        this.candidates = candidates;
-    }
-
-    @PostConstruct
-    public void load() {
-        List<UserAccount> reps = new ArrayList<>();
-        AccessGroup accessGroup = accessGroupBean.findAccessGroupByName("partners");
-        List<UserAccount> usersGroup = userGroupBean.findUsersGroup(accessGroup);
-
-        if (this.id != null && !this.id.isEmpty()) {
-            this.partner = partnerBean.find(id);
-
-            locationMBean.initialize();
-
-            if (this.partner.getAddress().getCountry() != null) {
-                locationMBean.setSelectedCountry(this.partner.getAddress().getCountry().getAcronym());
-            }
-
-            if (this.partner.getAddress().getProvince() != null) {
-                locationMBean.setSelectedProvince(this.partner.getAddress().getProvince().getId());
-            }
-
-            if (this.partner.getAddress().getCity() != null) {
-                locationMBean.setSelectedCity(this.partner.getAddress().getCity().getId());
-            }
-
-            reps.addAll(representativeBean.findRepresentativePersons(this.partner));
-            usersGroup.removeAll(reps);
-        } else {
-            this.partner = new Partner().setAddress(new Address());
-        }
-        this.candidates = new DualListModel<>(usersGroup, reps);
-    }
-
-    public String save() {
-        Country country = this.locationMBean.getCountry();
-        if (country != null) {
-            this.partner.getAddress().setCountry(country);
-        }
-
-        Province province = this.locationMBean.getProvince();
-        if (province != null) {
-            this.partner.getAddress().setProvince(province);
-        }
-
-        City city = this.locationMBean.getCity();
-        if (city != null) {
-            this.partner.getAddress().setCity(city);
-        }
-
-        List<UserAccount> reps = new ArrayList<>();
-        List<UserAccount> selectedCandidates = this.candidates.getTarget();
-        for (UserAccount selectedCandidate : selectedCandidates) {
-            reps.add(new UserAccount(selectedCandidate.getId()));
-        }
-
-        representativeBean.save(this.partner, reps);
-
-        return "partners?faces-redirect=true";
-    }
-
-    public String remove() {
-        partnerBean.remove(this.partner.getId());
-        return "partners?faces-redirect=true";
-    }
+  String remove() {
+    partnerBean.remove(partner.id)
+    'partners?faces-redirect=true'
+  }
 }
