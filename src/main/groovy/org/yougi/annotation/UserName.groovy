76b708809e7d7e2b0455320a169fb9302f04cfbd
eprogramming <00hf11@gmail.com>
2015-04-25 21:06:22 -0300

    #27

--- src/main/groovy/org/yougi/annotation/UserName.groovy
+++ src/main/groovy/org/yougi/annotation/UserName.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,14 +18,14 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.annotation;
+package org.yougi.annotation
 
-import javax.inject.Qualifier;
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
+import javax.inject.Qualifier
+import java.lang.annotation.Documented
+import java.lang.annotation.Retention
+import java.lang.annotation.Target
 import java.lang.annotation.ElementType
-import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.RetentionPolicy
 
 /**
  * @author Daniel Cunha - danielsoro@gmail.com
@@ -34,5 +34,5 @@ import java.lang.annotation.RetentionPolicy;
 @Target([ElementType.TYPE, ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD])
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface UserName {
+@interface UserName {
 }
