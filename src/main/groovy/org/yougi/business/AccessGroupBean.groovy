3ed87b9c3d3c8f0cb04da42739c6091cf3ab6e6e
eprogramming <00hf11@gmail.com>
2015-04-25 21:22:55 -0300

    #27

--- src/main/groovy/org/yougi/business/AccessGroupBean.groovy
+++ src/main/groovy/org/yougi/business/AccessGroupBean.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,156 +18,152 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.business;
+package org.yougi.business
 
-import org.yougi.entity.*;
-import org.yougi.exception.BusinessLogicException;
-import org.yougi.util.EntitySupport;
+import org.yougi.entity.*
+import org.yougi.exception.BusinessLogicException
+import org.yougi.util.EntitySupport
 
-import javax.ejb.EJB;
-import javax.ejb.Stateless;
-import javax.mail.MessagingException;
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.ejb.EJB
+import javax.ejb.Stateless
+import javax.mail.MessagingException
+import javax.persistence.EntityManager
+import javax.persistence.NoResultException
+import javax.persistence.PersistenceContext
+import javax.persistence.PersistenceException
+import java.util.ArrayList
+import java.util.List
+import java.util.logging.Level
+import java.util.logging.Logger
 
 /**
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
 @Stateless
-public class AccessGroupBean extends AbstractBean<AccessGroup> {
+class AccessGroupBean extends AbstractBean<AccessGroup> {
 
-    private static final Logger LOGGER = Logger.getLogger(AccessGroupBean.class.getSimpleName());
+  private static final Logger LOGGER = Logger.getLogger(AccessGroupBean.class.getSimpleName())
 
-    @PersistenceContext
-    private EntityManager em;
+  @PersistenceContext
+  private EntityManager em
+  @EJB
+  private AuthenticationBean authenticationBean
+  @EJB
+  private UserGroupBean userGroupBean
+  @EJB
+  private MessengerBean messengerBean
+  @EJB
+  private MessageTemplateBean messageTemplateBean
 
-    @EJB
-    private AuthenticationBean authenticationBean;
+  public static final String ADMIN_GROUP = 'admins'
+  public static final String DEFAULT_GROUP = 'members'
 
-    @EJB
-    private UserGroupBean userGroupBean;
+  AccessGroupBean() {
+    super(AccessGroup)
+  }
 
-    @EJB
-    private MessengerBean messengerBean;
+  @Override
+  protected EntityManager getEntityManager() {
+    em
+  }
 
-    @EJB
-    private MessageTemplateBean messageTemplateBean;
+  AccessGroup findAccessGroupByName(String name) {
+    AccessGroup ag
+    try {
+      def jpql = 'select ag from AccessGroup ag where ag.name = :name'
+      ag = em.createQuery(jpql, AccessGroup).setParameter('name', name).getSingleResult()
+    } catch(NoResultException nre) {
+      LOGGER.log(Level.INFO, nre.getMessage())
+    }
+    ag
+  }
 
-    public static final String ADMIN_GROUP = "admins";
-    public static final String DEFAULT_GROUP = "members";
+  AccessGroup findDefaultAccessGroup() {
+    AccessGroup defaultUserGroup
+    try {
+      defaultUserGroup = em.createQuery('select ag from AccessGroup ag where ag.userDefault = :default', AccessGroup)
+        .setParameter('default', Boolean.TRUE)
+        .getSingleResult()
+    } catch(NoResultException nre) {
+      LOGGER.log(Level.INFO, nre.getMessage())
+      defaultUserGroup = new AccessGroup(DEFAULT_GROUP,'Default Members Group')
+      defaultUserGroup.setId(EntitySupport.generateEntityId())
+      defaultUserGroup.setUserDefault(Boolean.TRUE)
+      em.persist(defaultUserGroup)
+    }
+    defaultUserGroup
+  }
 
-    public AccessGroupBean() {
-        super(AccessGroup.class);
+  /** Returns the existing administrative group. If it doesn't find anyone
+   *  then a new one is created and returned. */
+  AccessGroup findAdministrativeGroup() {
+    AccessGroup group
+    try {
+      group = em.createQuery('select ag from AccessGroup ag where ag.name = :name', AccessGroup)
+        .setParameter('name', ADMIN_GROUP)
+        .getSingleResult()
+    } catch(NoResultException nre) {
+      LOGGER.log(Level.INFO, nre.getMessage())
+      group = new AccessGroup(ADMIN_GROUP,'Administrators Group')
+      group.setId(EntitySupport.generateEntityId())
+      em.persist(group)
+    }
+    group
+  }
+
+  List<AccessGroup> findAccessGroups() {
+    em.createQuery('select ag from AccessGroup ag order by ag.name', AccessGroup).getResultList()
+  }
+
+  void sendGroupAssignmentAlert(UserAccount userAccount, AccessGroup accessGroup) throws BusinessLogicException {
+    MessageTemplate messageTemplate = messageTemplateBean.find('09JDIIE82O39IDIDOSJCHXUDJJXHCKP0')
+    messageTemplate.setVariable('userAccount.firstName', userAccount.firstName)
+    messageTemplate.setVariable('accessGroup.name', accessGroup.name)
+    EmailMessage emailMessage = messageTemplate.buildEmailMessage()
+    emailMessage.setRecipient(userAccount)
+
+    try {
+      messengerBean.sendEmailMessage(emailMessage)
+    } catch(MessagingException me) {
+      LOGGER.log(Level.WARNING, 'Error when sending the group assignment alert to '+ userAccount.fullName, me)
+    }
+  }
+
+  @Override
+  AccessGroup save(AccessGroup entity) {
+    throw new BusinessLogicException('Please use the save method that accepts the list of members by parameter')
+  }
+
+  AccessGroup save(AccessGroup accessGroup, List<UserAccount> members) {
+    if (accessGroup.getUserDefault()) {
+      AccessGroup defaultGroup = findDefaultAccessGroup()
+      defaultGroup.setUserDefault(false)
     }
 
-    @Override
-    protected EntityManager getEntityManager() {
-        return em;
-    }
-
-    public AccessGroup findAccessGroupByName(String name) {
-        try {
-            return em.createQuery("select ag from AccessGroup ag where ag.name = :name", AccessGroup.class)
-                     .setParameter("name", name)
-                     .getSingleResult();
-        } catch(NoResultException nre) {
-            LOGGER.log(Level.INFO, nre.getMessage());
-            return null;
+    if (EntitySupport.isIdValid(accessGroup.id)) {
+      try {
+        AccessGroup group = findAccessGroupByName(accessGroup.name)
+        if (group) {
+          throw new PersistenceException('A group named '+ accessGroup.name +' already exists.')
         }
+      } catch(NoResultException nre) {
+        LOGGER.log(Level.INFO, nre.message)
+        accessGroup.id = EntitySupport.generateEntityId()
+        em.persist(accessGroup)
+      }
+    } else {
+      accessGroup = em.merge(accessGroup)
     }
 
-    public AccessGroup findDefaultAccessGroup() {
-        AccessGroup defaultUserGroup;
-        try {
-            defaultUserGroup = em.createQuery("select ag from AccessGroup ag where ag.userDefault = :default", AccessGroup.class)
-                                        .setParameter("default", Boolean.TRUE)
-                                        .getSingleResult();
-        } catch(NoResultException nre) {
-            LOGGER.log(Level.INFO, nre.getMessage());
-            defaultUserGroup = new AccessGroup(DEFAULT_GROUP,"Default Members Group");
-            defaultUserGroup.setId(EntitySupport.generateEntityId());
-            defaultUserGroup.setUserDefault(Boolean.TRUE);
-            em.persist(defaultUserGroup);
-        }
-        return defaultUserGroup;
+    if (members) {
+      Authentication auth
+      List<UserGroup> usersGroup = new ArrayList<>()
+      for (UserAccount member: members) {
+        auth = authenticationBean.findByUserId(member.id)
+        usersGroup.add(new UserGroup(accessGroup, auth))
+      }
+      userGroupBean.update(accessGroup, usersGroup)
     }
-
-    /** Returns the existing administrative group. If it doesn't find anyone
-     *  then a new one is created and returned. */
-    public AccessGroup findAdministrativeGroup() {
-        AccessGroup group;
-        try {
-            group = em.createQuery("select ag from AccessGroup ag where ag.name = :name", AccessGroup.class)
-                      .setParameter("name", ADMIN_GROUP)
-                      .getSingleResult();
-        } catch(NoResultException nre) {
-            LOGGER.log(Level.INFO, nre.getMessage());
-            group = new AccessGroup(ADMIN_GROUP,"Administrators Group");
-            group.setId(EntitySupport.generateEntityId());
-            em.persist(group);
-        }
-        return group;
-    }
-
-    public List<AccessGroup> findAccessGroups() {
-        return em.createQuery("select ag from AccessGroup ag order by ag.name", AccessGroup.class).getResultList();
-    }
-
-    public void sendGroupAssignmentAlert(UserAccount userAccount, AccessGroup accessGroup) throws BusinessLogicException {
-        MessageTemplate messageTemplate = messageTemplateBean.find("09JDIIE82O39IDIDOSJCHXUDJJXHCKP0");
-        messageTemplate.setVariable("userAccount.firstName", userAccount.getFirstName());
-        messageTemplate.setVariable("accessGroup.name", accessGroup.getName());
-        EmailMessage emailMessage = messageTemplate.buildEmailMessage();
-        emailMessage.setRecipient(userAccount);
-
-        try {
-            messengerBean.sendEmailMessage(emailMessage);
-        } catch(MessagingException me) {
-            LOGGER.log(Level.WARNING, "Error when sending the group assignment alert to "+ userAccount.getFullName(), me);
-        }
-    }
-
-    @Override
-    public AccessGroup save(AccessGroup entity) {
-        throw new BusinessLogicException("Please use the save method that accepts the list of members by parameter");
-    }
-
-    public AccessGroup save(AccessGroup accessGroup, List<UserAccount> members) {
-        if(accessGroup.getUserDefault()) {
-            AccessGroup defaultGroup = findDefaultAccessGroup();
-            defaultGroup.setUserDefault(false);
-        }
-
-        if(EntitySupport.isIdValid(accessGroup.getId())) {
-            try {
-                AccessGroup group = findAccessGroupByName(accessGroup.getName());
-                if(group != null) {
-                    throw new PersistenceException("A group named '"+ accessGroup.getName() +"' already exists.");
-                }
-            } catch(NoResultException nre) {
-                LOGGER.log(Level.INFO, nre.getMessage());
-                accessGroup.setId(EntitySupport.generateEntityId());
-                em.persist(accessGroup);
-            }
-        } else {
-            accessGroup = em.merge(accessGroup);
-        }
-
-        if(members != null) {
-            Authentication auth;
-            List<UserGroup> usersGroup = new ArrayList<>();
-            for(UserAccount member: members) {
-                auth = authenticationBean.findByUserId(member.getId());
-                usersGroup.add(new UserGroup(accessGroup, auth));
-            }
-            userGroupBean.update(accessGroup, usersGroup);
-        }
-        return accessGroup;
-    }
+    accessGroup
+  }
 }
