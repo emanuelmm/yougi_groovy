ecb9fd962e619e9b6bc2623e414f5f843d2118c6
eprogramming <00hf11@gmail.com>
2015-04-25 22:14:25 -0300

    #27

--- src/main/groovy/org/yougi/entity/Address.groovy
+++ src/main/groovy/org/yougi/entity/Address.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,122 +18,78 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.entity;
+package org.yougi.entity
 
-import javax.persistence.Access;
-import javax.persistence.AccessType;
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.Access
+import javax.persistence.AccessType
+import javax.persistence.Column
+import javax.persistence.Embeddable
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 
 /**
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
 @Embeddable
 @Access(AccessType.FIELD)
-public class Address {
+class Address {
 
-    private String address;
+  String address
+  @ManyToOne
+  @JoinColumn(name = 'city')
+  City city
 
-    @ManyToOne
-    @JoinColumn(name = "city")
-    private City city;
+  @ManyToOne
+  @JoinColumn(name = 'province')
+  Province province
 
-    @ManyToOne
-    @JoinColumn(name = "province")
-    private Province province;
+  @ManyToOne
+  @JoinColumn(name = 'country')
+  Country country
 
-    @ManyToOne
-    @JoinColumn(name = "country")
-    private Country country;
+  @Column(name = 'postal_code')
+  String postalCode
 
-    @Column(name = "postal_code")
-    private String postalCode;
-
-    public String getAddress() {
-        return address;
+  @Override
+  String toString() {
+    def fullAddress = ''
+    def commaSeparator = ', '
+    if (address) {
+      fullAddress <<= address
     }
 
-    public void setAddress(String address) {
-        this.address = address;
+    if (city) {
+      if (fullAddress) {
+        fullAddress <<= commaSeparator
+      }
+      fullAddress <<= city.name
     }
 
-    public City getCity() {
-        return city;
+    if (province) {
+      if (fullAddress) {
+        fullAddress <<= commaSeparator
+      }
+      fullAddress <<= province.name
     }
 
-    public void setCity(City city) {
-        this.city = city;
+    if (country) {
+      if (fullAddress) {
+        fullAddress <<= ' - '
+      }
+      fullAddress <<= country.name
     }
 
-    public Province getProvince() {
-        return province;
+    if (postalCode) {
+      if (fullAddress) {
+        fullAddress <<= '.'
+      }
+      fullAddress <<= ' '
+      if (country) {
+        fullAddress <<= ': '
+        fullAddress <<= country.name
+      }
     }
 
-    public void setProvince(Province province) {
-        this.province = province;
-    }
-
-    public Country getCountry() {
-        return country;
-    }
-
-    public void setCountry(Country country) {
-        this.country = country;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder fullAddress = new StringBuilder();
-        String commaSeparator = ", ";
-        if (address != null && !address.isEmpty()) {
-            fullAddress.append(address);
-        }
-
-        if (city != null) {
-            if (!fullAddress.toString().isEmpty()) {
-                fullAddress.append(commaSeparator);
-            }
-
-            fullAddress.append(city.getName());
-        }
-
-        if (province != null) {
-            if (!fullAddress.toString().isEmpty()) {
-                fullAddress.append(commaSeparator);
-            }
-
-            fullAddress.append(province.getName());
-        }
-
-        if (country != null) {
-            if (!fullAddress.toString().isEmpty()) {
-                fullAddress.append(" - ");
-            }
-
-            fullAddress.append(country.getName());
-        }
-
-        if (postalCode != null) {
-            if (!fullAddress.toString().isEmpty()) {
-                fullAddress.append(".");
-            }
-            fullAddress.append(" ");
-            if (country != null) {
-                fullAddress.append(": ");
-                fullAddress.append(country.getName());
-            }
-        }
-
-        return fullAddress.toString();
-    }
+    fullAddress
+  }
 }
