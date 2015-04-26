2c36d6dc8abb4b2ead859c1aa8e886dbcdc57168
eprogramming <00hf11@gmail.com>
2015-04-26 15:10:44 -0300

    build fix

--- src/main/groovy/org/yougi/business/GenericPersistence.groovy
+++ src/main/groovy/org/yougi/business/GenericPersistence.groovy
@@ -30,6 +30,7 @@ import javax.persistence.criteria.CriteriaBuilder
 import javax.persistence.criteria.CriteriaQuery
 import javax.persistence.criteria.Order
 import javax.persistence.criteria.Root
+import javax.persistence.NoResultException
 
 @Transactional
 class GenericPersistence implements Serializable {
