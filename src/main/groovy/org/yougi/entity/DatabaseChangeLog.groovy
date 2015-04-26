d61cd3c265c335e7c7a5191f451d29fffb6c2312
eprogramming <00hf11@gmail.com>
2015-04-25 22:42:50 -0300

    #27

--- src/main/groovy/org/yougi/entity/DatabaseChangeLog.groovy
+++ src/main/groovy/org/yougi/entity/DatabaseChangeLog.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,162 +18,39 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.entity;
+package org.yougi.entity
 
-import javax.persistence.*;
-import java.io.Serializable;
-import java.util.Date;
+import javax.persistence.*
+import java.io.Serializable
 
 /**
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
 @Entity
-@Table(name = "DATABASECHANGELOG")
-public class DatabaseChangeLog implements Serializable, Identified {
-    private static final long serialVersionUID = 1L;
+@Table(name = 'DATABASECHANGELOG')
+class DatabaseChangeLog implements Serializable, Identified {
 
-    @Id
-    private String id;
+  @Id
+  String id
+  String author
+  String filename
 
-    private String author;
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = 'dateexecuted')
+  Date dateExecuted
 
-    private String filename;
+  @Column(name = 'orderexecuted')
+  Integer orderExecuted
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "dateexecuted")
-    private Date dateExecuted;
+  @Column(name = 'exectype')
+  String executionType
+  String md5sum
+  String description
+  String comments
+  String tag
+  String liquibase
 
-    @Column(name = "orderexecuted")
-    private Integer orderExecuted;
+  @Transient
+  String changesContent
 
-    @Column(name = "exectype")
-    private String executionType;
-
-    private String md5sum;
-
-    private String description;
-
-    private String comments;
-
-    private String tag;
-
-    private String liquibase;
-
-    @Transient
-    private String changesContent;
-
-    @Override
-    public String getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getFilename() {
-        return filename;
-    }
-
-    public void setFilename(String filename) {
-        this.filename = filename;
-    }
-
-    public Date getDateExecuted() {
-        return dateExecuted;
-    }
-
-    public void setDateExecuted(Date dateExecuted) {
-        this.dateExecuted = dateExecuted;
-    }
-
-    public Integer getOrderExecuted() {
-        return orderExecuted;
-    }
-
-    public void setOrderExecuted(Integer orderExecuted) {
-        this.orderExecuted = orderExecuted;
-    }
-
-    public String getExecutionType() {
-        return executionType;
-    }
-
-    public void setExecutionType(String executionType) {
-        this.executionType = executionType;
-    }
-
-    public String getMd5sum() {
-        return md5sum.toUpperCase();
-    }
-
-    public void setMd5sum(String md5sum) {
-        this.md5sum = md5sum;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-
-    public String getTag() {
-        return tag;
-    }
-
-    public void setTag(String tag) {
-        this.tag = tag;
-    }
-
-    public String getLiquibase() {
-        return liquibase;
-    }
-
-    public void setLiquibase(String liquibase) {
-        this.liquibase = liquibase;
-    }
-
-    public String getChangesContent() {
-        return changesContent;
-    }
-
-    public void setChangesContent(String changesContent) {
-        this.changesContent = changesContent;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        DatabaseChangeLog that = (DatabaseChangeLog) o;
-
-        if (!id.equals(that.id)) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return id.hashCode();
-    }
 }
