feb1a5e669fba235c7b0c51a8279704f49094231
eprogramming <00hf11@gmail.com>
2015-04-26 02:01:51 -0300

    #27

--- src/main/groovy/org/yougi/web/handler/CustomExceptionHandler.groovy
+++ src/main/groovy/org/yougi/web/handler/CustomExceptionHandler.groovy
@@ -1,75 +1,74 @@
-/* Yougi is a web application conceived to manage user groups or 
- * communities focused on a certain domain of knowledge, whose members are 
- * constantly sharing information and participating in social and educational 
+/* Yougi is a web application conceived to manage user groups or
+ * communities focused on a certain domain of knowledge, whose members are
+ * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
- * 
- * This application is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by the 
- * Free Software Foundation; either version 2.1 of the License, or (at your 
+ *
+ * This application is free software you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
- * 
- * This application is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ *
+ * This application is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
- * 
- * There is a full copy of the GNU Lesser General Public License along with 
+ *
+ * There is a full copy of the GNU Lesser General Public License along with
  * this library. Look for the file license.txt at the root level. If you do not
- * find it, write to the Free Software Foundation, Inc., 59 Temple Place, 
+ * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.web.handler;
+package org.yougi.web.handler
 
-import org.yougi.util.ResourceBundleHelper;
+import org.yougi.util.ResourceBundleHelper
 
-import javax.faces.application.Application;
-import javax.faces.application.NavigationHandler;
-import javax.faces.application.ViewExpiredException;
-import javax.faces.context.ExceptionHandler;
-import javax.faces.context.ExceptionHandlerWrapper;
-import javax.faces.context.FacesContext;
-import javax.faces.event.ExceptionQueuedEvent;
-import javax.faces.event.ExceptionQueuedEventContext;
+import javax.faces.application.Application
+import javax.faces.application.NavigationHandler
+import javax.faces.application.ViewExpiredException
+import javax.faces.context.ExceptionHandler
+import javax.faces.context.ExceptionHandlerWrapper
+import javax.faces.context.FacesContext
+import javax.faces.event.ExceptionQueuedEvent
+import javax.faces.event.ExceptionQueuedEventContext
 
-public class CustomExceptionHandler extends ExceptionHandlerWrapper {
+class CustomExceptionHandler extends ExceptionHandlerWrapper {
 
-    private ExceptionHandler parent;
+  private ExceptionHandler parent
+  CustomExceptionHandler() {}
 
-    public CustomExceptionHandler() {}
+  CustomExceptionHandler(ExceptionHandler parent) {
+    this.parent = parent
+  }
 
-    public CustomExceptionHandler(ExceptionHandler parent) {
-        this.parent = parent;
-    }
+  @Override
+  ExceptionHandler getWrapped() {
+    parent
+  }
 
-    @Override
-    public ExceptionHandler getWrapped() {
-        return this.parent;
-    }
-
-    @Override
-    public void handle() {
-        ExceptionQueuedEvent event;
-        ExceptionQueuedEventContext eventContext;
-        Throwable t;
-        for (Iterator<ExceptionQueuedEvent> i = getUnhandledExceptionQueuedEvents().iterator(); i.hasNext();) {
-            event = i.next();
-            eventContext = (ExceptionQueuedEventContext) event.getSource();
-            t = eventContext.getException();
-            if (t instanceof ViewExpiredException) {
-                FacesContext context = FacesContext.getCurrentInstance();
-                Application app = context.getApplication();
-                if(app != null) {
-                    NavigationHandler nav = app.getNavigationHandler();
-                    try {
-                        context.getExternalContext().getFlash().put("currentViewId", ResourceBundleHelper.getMessage("warnCode0004"));
-                        nav.handleNavigation(context, null, "/login?faces-redirect=true");
-                        context.renderResponse();
-                    } finally {
-                        i.remove();
-                    }
-                }
-            }
+  @Override
+  void handle() {
+    ExceptionQueuedEvent event
+    ExceptionQueuedEventContext eventContext
+    Throwable t
+    for (Iterator<ExceptionQueuedEvent> i = getUnhandledExceptionQueuedEvents().iterator(); i.hasNext();) {
+      event = i.next()
+      eventContext = (ExceptionQueuedEventContext) event.getSource()
+      t = eventContext.getException()
+      if (t instanceof ViewExpiredException) {
+        FacesContext context = FacesContext.getCurrentInstance()
+        Application app = context.getApplication()
+        if(app != null) {
+          NavigationHandler nav = app.getNavigationHandler()
+          try {
+            context.getExternalContext().getFlash().put("currentViewId", ResourceBundleHelper.getMessage("warnCode0004"))
+            nav.handleNavigation(context, null, "/login?faces-redirect=true")
+            context.renderResponse()
+          } finally {
+            i.remove()
+          }
         }
-        getWrapped().handle();
+      }
     }
+    getWrapped().handle()
+  }
 }
