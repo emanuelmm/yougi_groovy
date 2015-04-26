74a55c2caecf5b9fd0c63b43d30a7bda02ea402a
eprogramming <00hf11@gmail.com>
2015-04-26 13:14:56 -0300

    #27

--- src/main/groovy/org/yougi/util/PackageResourceHelper.groovy
+++ src/main/groovy/org/yougi/util/PackageResourceHelper.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,70 +18,65 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.util;
+package org.yougi.util
 
-import org.jboss.vfs.TempFileProvider;
-import org.jboss.vfs.VFS;
-import org.jboss.vfs.VirtualFile;
-import org.yougi.business.JobSchedulerBean;
-import org.yougi.exception.EnvironmentResourceException;
+import org.jboss.vfs.TempFileProvider
+import org.jboss.vfs.VFS
+import org.jboss.vfs.VirtualFile
+import org.yougi.business.JobSchedulerBean
+import org.yougi.exception.EnvironmentResourceException
 
-import java.util.concurrent.Executors;
+import java.util.concurrent.Executors
 
 /**
  * Encapsulates the complexity of getting resources from the war file on runtime.
  *
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
-public class PackageResourceHelper {
+class PackageResourceHelper {
 
-    private PackageResourceHelper() {}
+  private PackageResourceHelper() {}
 
-    //public static File getFile() {
+  static List<File> getFilesFolder(String pathFolder) {
+    URL url = getResourceUrl(pathFolder)
 
-    //}
-
-    public static List<File> getFilesFolder(String pathFolder) {
-        URL url = getResourceUrl(pathFolder);
-
-        if (url == null) {
-            return Collections.emptyList();
-        }
-
-        VirtualFile virtualFile;
-        Closeable handle = null;
-        String protocol = url.getProtocol();
-        List<File> files = new ArrayList<>();
-        try {
-            if ("vfs".equals(protocol)) {
-                URLConnection conn = url.openConnection();
-                virtualFile = (VirtualFile) conn.getContent();
-            } else if ("file".equals(protocol)) {
-                virtualFile = VFS.getChild(url.toURI());
-                File archiveFile = virtualFile.getPhysicalFile();
-                TempFileProvider provider = TempFileProvider.create("tmp", Executors.newScheduledThreadPool(2));
-                handle = VFS.mountZip(archiveFile, virtualFile, provider);
-            } else {
-                throw new UnsupportedOperationException("Protocol " + protocol + " is not supported");
-            }
-
-            List<VirtualFile> virtualFiles = virtualFile.getChildren();
-            for(VirtualFile vFile : virtualFiles) {
-                files.add(vFile.getPhysicalFile());
-            }
-
-            if(handle != null) {
-                handle.close();
-            }
-        } catch (IOException | URISyntaxException ioe) {
-            throw new EnvironmentResourceException(ioe.getMessage(), ioe);
-        }
-
-        return files;
+    if (url == null) {
+      return Collections.emptyList()
     }
 
-    private static URL getResourceUrl(String path) {
-        final ClassLoader loader = PackageResourceHelper.class.getClassLoader();
-        return loader.getResource(path);
+    VirtualFile virtualFile
+    Closeable handle = null
+    String protocol = url.getProtocol()
+    List<File> files = new ArrayList<>()
+    try {
+      if ('vfs'.equals(protocol)) {
+        URLConnection conn = url.openConnection()
+        virtualFile = (VirtualFile) conn.getContent()
+      } else if ('file'.equals(protocol)) {
+        virtualFile = VFS.getChild(url.toURI())
+        File archiveFile = virtualFile.getPhysicalFile()
+        TempFileProvider provider = TempFileProvider.create('tmp', Executors.newScheduledThreadPool(2))
+        handle = VFS.mountZip(archiveFile, virtualFile, provider)
+      } else {
+        throw new UnsupportedOperationException('Protocol ' + protocol + ' is not supported')
+      }
+
+      List<VirtualFile> virtualFiles = virtualFile.getChildren()
+      for(VirtualFile vFile : virtualFiles) {
+        files.add(vFile.getPhysicalFile())
+      }
+
+      if(handle) {
+        handle.close()
+      }
+    } catch (e) {
+      throw new EnvironmentResourceException(e.message, e)
     }
+    files
+  }
+
+  private static URL getResourceUrl(String path) {
+    final ClassLoader loader = PackageResourceHelper.class.getClassLoader()
+    loader.getResource(path)
+  }
 }
