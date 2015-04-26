7dd0371acfd6e3b7a8360f14a0cc74c4b151ecf3
eprogramming <00hf11@gmail.com>
2015-04-25 22:46:02 -0300

    #27

--- src/main/groovy/org/yougi/entity/JobExecution.groovy
+++ src/main/groovy/org/yougi/entity/JobExecution.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,144 +18,50 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.entity;
+package org.yougi.entity
 
-import org.yougi.reference.JobStatus;
+import org.yougi.reference.JobStatus
 
-import javax.persistence.*;
-import java.io.Serializable;
-import java.util.Date;
-import java.util.logging.Logger;
+import javax.persistence.*
+import java.io.Serializable
 
 /**
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
 @Entity
-@Table(name = "job_execution")
-public class JobExecution implements Serializable, Identified {
-    private static final long serialVersionUID = 1L;
+@Table(name = 'job_execution')
+class JobExecution implements Serializable, Identified {
 
-    private static final Logger LOGGER = Logger.getLogger(JobExecution.class.getSimpleName());
+  @Id
+  String id
 
-    @Id
-    private String id;
+  @ManyToOne
+  @JoinColumn(name = 'job_scheduler')
+  JobScheduler jobScheduler
 
-    @ManyToOne
-    @JoinColumn(name = "job_scheduler")
-    private JobScheduler jobScheduler;
+  @ManyToOne
+  @JoinColumn(name = 'owner')
+  UserAccount owner
 
-    @ManyToOne
-    @JoinColumn(name = "owner")
-    private UserAccount owner;
+  @Column(name = 'instance_id')
+  Long instanceId
 
-    @Column(name = "instance_id")
-    private Long instanceId;
+  @Enumerated(EnumType.STRING)
+  JobStatus status
 
-    @Enumerated(EnumType.STRING)
-    private JobStatus status;
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = 'start_time')
+  Date startTime
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "start_time")
-    private Date startTime;
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = 'end_time')
+  Date endTime
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @Column(name = "end_time")
-    private Date endTime;
+  @Transient
+  Date timeout
 
-    @Transient
-    private Date timeout;
+  String toString() {
+    jobScheduler.getFrequencyType() + ' to start at ' + startTime
+  }
 
-    public JobExecution() {}
-
-    public JobExecution(JobScheduler jobScheduler, UserAccount owner, Date startTime) {
-        this.jobScheduler = jobScheduler;
-        this.owner = owner;
-        this.startTime = startTime;
-        this.status = JobStatus.SCHEDULED;
-    }
-
-    @Override
-    public String getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public JobScheduler getJobScheduler() {
-        return jobScheduler;
-    }
-
-    public UserAccount getOwner() {
-        return owner;
-    }
-
-    public Long getInstanceId() {
-        return instanceId;
-    }
-
-    public void setInstanceId(Long instanceId) {
-        this.instanceId = instanceId;
-    }
-
-    public JobStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(JobStatus status) {
-        this.status = status;
-    }
-
-    public Date getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Date startTime) {
-        this.startTime = startTime;
-    }
-
-    public Date getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(Date endTime) {
-        this.endTime = endTime;
-    }
-
-    public Date getTimeout() {
-        return timeout;
-    }
-
-    public void setTimeout(Date timeout) {
-        this.timeout = timeout;
-    }
-
-    public String toString() {
-        return this.jobScheduler.getFrequencyType() + " to start at " + this.startTime;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof JobExecution)) {
-            return false;
-        }
-
-        JobExecution that = (JobExecution) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) {
-            return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
-    }
 }
