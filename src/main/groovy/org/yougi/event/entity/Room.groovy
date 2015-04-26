bb98466f6cfbe9403a00d6ea4f34f43b98384ac6
eprogramming <00hf11@gmail.com>
2015-04-25 23:22:24 -0300

    #27

--- src/main/groovy/org/yougi/event/entity/Room.groovy
+++ src/main/groovy/org/yougi/event/entity/Room.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,97 +18,32 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.event.entity;
+package org.yougi.event.entity
 
-import org.yougi.entity.Identified;
+import org.yougi.entity.Identified
 
-import javax.persistence.*;
-import java.io.Serializable;
+import javax.persistence.*
+import java.io.Serializable
 
 /**
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
 @Entity
-@Table(name="room")
-public class Room implements Serializable, Identified {
-    private static final long serialVersionUID = 1L;
+@Table(name='room')
+class Room implements Serializable, Identified {
 
-    @Id
-    private String id;
+  @Id
+  String id
+  String name
+  String description
+  Integer capacity
 
-    private String name;
+  @ManyToOne
+  @JoinColumn(name = 'venue', nullable = false)
+  Venue venue
 
-    private String description;
-
-    private Integer capacity;
-
-    @ManyToOne
-    @JoinColumn(name = "venue", nullable = false)
-    private Venue venue;
-
-    @Override
-    public String getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(Integer capacity) {
-        this.capacity = capacity;
-    }
-
-    public Venue getVenue() {
-        return venue;
-    }
-
-    public void setVenue(Venue venue) {
-        this.venue = venue;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (!(object instanceof Room)) {
-            return false;
-        }
-        Room other = (Room) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return this.name;
-    }
+  @Override
+  String toString() {
+    name
+  }
 }
