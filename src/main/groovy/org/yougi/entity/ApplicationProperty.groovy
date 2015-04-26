c3cea6c1b9b1541a637b107ec4739cc5c4221b74
eprogramming <00hf11@gmail.com>
2015-04-25 22:17:10 -0300

    #27

--- src/main/groovy/org/yougi/entity/ApplicationProperty.groovy
+++ src/main/groovy/org/yougi/entity/ApplicationProperty.groovy
@@ -1,103 +1,57 @@
-/* Yougi is a web application conceived to manage user groups or 
- * communities focused on a certain domain of knowledge, whose members are 
- * constantly sharing information and participating in social and educational 
+/* Yougi is a web application conceived to manage user groups or
+ * communities focused on a certain domain of knowledge, whose members are
+ * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
- * 
- * This application is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU Lesser General Public License as published by the 
- * Free Software Foundation; either version 2.1 of the License, or (at your 
+ *
+ * This application is free software you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
- * 
- * This application is distributed in the hope that it will be useful, but 
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ *
+ * This application is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
- * 
- * There is a full copy of the GNU Lesser General Public License along with 
+ *
+ * There is a full copy of the GNU Lesser General Public License along with
  * this library. Look for the file license.txt at the root level. If you do not
- * find it, write to the Free Software Foundation, Inc., 59 Temple Place, 
+ * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.entity;
+package org.yougi.entity
 
-import org.yougi.reference.Properties;
+import org.yougi.reference.Properties
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import java.io.Serializable;
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+import java.io.Serializable
 
 /**
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
 @Entity
-@Table(name="application_property")
-public class ApplicationProperty implements Serializable {
-    private static final long serialVersionUID = 1L;
+@Table(name='application_property')
+class ApplicationProperty implements Serializable {
 
-    @Id
-    @Column(name="property_key", nullable=false)
-    private String propertyKey;
+  @Id
+  @Column(name='property_key', nullable=false)
+  String propertyKey
 
-    @Column(name="property_value")
-    private String propertyValue;
+  @Column(name='property_value')
+  String propertyValue
 
-    public ApplicationProperty() {}
-
-    public ApplicationProperty(String propertyKey, String propertyValue) {
-        this.propertyKey = propertyKey;
-        this.propertyValue = propertyValue;
+  boolean sendEmailsEnabled() {
+    if(propertyKey.equals(Properties.SEND_EMAILS.getKey())) {
+      return Boolean.valueOf(propertyValue)
+    } else {
+      return false
     }
+  }
 
-    public String getPropertyKey() {
-        return propertyKey;
-    }
-
-    public void setPropertyKey(String propertyKey) {
-        this.propertyKey = propertyKey;
-    }
-
-    public String getPropertyValue() {
-        return propertyValue;
-    }
-
-    public void setPropertyValue(String propertyValue) {
-        this.propertyValue = propertyValue;
-    }
-
-    public boolean sendEmailsEnabled() {
-        if(propertyKey.equals(Properties.SEND_EMAILS.getKey())) {
-            return Boolean.valueOf(propertyValue);
-        } else {
-            return false;
-        }
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final ApplicationProperty other = (ApplicationProperty) obj;
-        if (this.propertyKey == null ? other.propertyKey != null : !this.propertyKey.equals(other.propertyKey)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 89 * hash + (this.propertyKey != null ? this.propertyKey.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public String toString() {
-        return propertyKey +" = "+ propertyValue;
-    }
+  @Override
+  String toString() {
+    propertyKey + ' = ' + propertyValue
+  }
 }
