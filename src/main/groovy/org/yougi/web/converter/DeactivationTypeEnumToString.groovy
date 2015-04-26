e9969335620e87f80c798b2c3522d14373288286
eprogramming <00hf11@gmail.com>
2015-04-26 02:03:07 -0300

    #27

--- src/main/groovy/org/yougi/web/converter/DeactivationTypeEnumToString.groovy
+++ src/main/groovy/org/yougi/web/converter/DeactivationTypeEnumToString.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,15 +18,15 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.web.converter;
+package org.yougi.web.converter
 
-import org.yougi.reference.DeactivationType;
-import org.yougi.util.ResourceBundleHelper;
+import org.yougi.reference.DeactivationType
+import org.yougi.util.ResourceBundleHelper
 
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-import javax.faces.convert.Converter;
-import javax.faces.convert.FacesConverter;
+import javax.faces.component.UIComponent
+import javax.faces.context.FacesContext
+import javax.faces.convert.Converter
+import javax.faces.convert.FacesConverter
 
 /**
  * DeactivationType is a Enum. This converter is responsible for transforming
@@ -34,31 +34,31 @@ import javax.faces.convert.FacesConverter;
  *
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
-@FacesConverter(value = "DeactivationTypeEnumToString")
-public class DeactivationTypeEnumToString implements Converter {
+@FacesConverter(value = 'DeactivationTypeEnumToString')
+class DeactivationTypeEnumToString implements Converter {
 
-    @Override
-    public Object getAsObject(FacesContext context, UIComponent component, String value) {
-        switch (value) {
-            case "ownWill":
-                return DeactivationType.OWNWILL;
-            case "administrative":
-                return DeactivationType.ADMINISTRATIVE;
-            default:
-                return null;
-        }
+  @Override
+  Object getAsObject(FacesContext context, UIComponent component, String value) {
+    switch (value) {
+      case 'ownWill':
+        return DeactivationType.OWNWILL
+      case 'administrative':
+        return DeactivationType.ADMINISTRATIVE
+      default:
+        return null
     }
+  }
 
-    @Override
-    public String getAsString(FacesContext context, UIComponent component, Object value) {
-        DeactivationType deactivationType = (DeactivationType) value;
-        switch (deactivationType) {
-            case OWNWILL:
-                return ResourceBundleHelper.getMessage("ownwill");
-            case ADMINISTRATIVE:
-                return ResourceBundleHelper.getMessage("administrative");
-            default:
-                return null;
-        }
+  @Override
+  String getAsString(FacesContext context, UIComponent component, Object value) {
+    DeactivationType deactivationType = (DeactivationType) value
+    switch (deactivationType) {
+      case OWNWILL:
+        return ResourceBundleHelper.getMessage('ownwill')
+      case ADMINISTRATIVE:
+        return ResourceBundleHelper.getMessage('administrative')
+      default:
+        return null
     }
+  }
 }
