18adcadbeb8ccfe386a048e4febee4899ddc8821
eprogramming <00hf11@gmail.com>
2015-04-26 00:30:03 -0300

    #27

--- src/main/groovy/org/yougi/event/entity/SpeakerSession.groovy
+++ src/main/groovy/org/yougi/event/entity/SpeakerSession.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,12 +18,12 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.event.entity;
+package org.yougi.event.entity
 
-import org.yougi.entity.Identified;
+import org.yougi.entity.Identified
 
-import javax.persistence.*;
-import java.io.Serializable;
+import javax.persistence.*
+import java.io.Serializable
 
 /**
  * Associates a speaker to a sessionEvent.
@@ -31,68 +31,22 @@ import java.io.Serializable;
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
 @Entity
-@Table(name = "speaker_session")
-public class SpeakerSession implements Serializable, Identified {
-    private static final long serialVersionUID = 1L;
+@Table(name = 'speaker_session')
+class SpeakerSession implements Serializable, Identified {
 
-    @Id
-    private String id;
+  @Id
+  String id
 
-    @ManyToOne
-    @JoinColumn(name = "speaker", nullable = false)
-    private Speaker speaker;
+  @ManyToOne
+  @JoinColumn(name = 'speaker', nullable = false)
+  Speaker speaker
 
-    @ManyToOne
-    @JoinColumn(name = "session", nullable = false)
-    private SessionEvent sessionEvent;
+  @ManyToOne
+  @JoinColumn(name = 'session', nullable = false)
+  SessionEvent sessionEvent
 
-    @Override
-    public String getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Speaker getSpeaker() {
-        return speaker;
-    }
-
-    public void setSpeaker(Speaker speaker) {
-        this.speaker = speaker;
-    }
-
-    public SessionEvent getSessionEvent() {
-        return sessionEvent;
-    }
-
-    public void setSessionEvent(SessionEvent sessionEvent) {
-        this.sessionEvent = sessionEvent;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (!(object instanceof SpeakerSession)) {
-            return false;
-        }
-        SpeakerSession other = (SpeakerSession) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return this.sessionEvent.getName() + " - " + this.speaker.getUserAccount().getFullName();
-    }
+  @Override
+  String toString() {
+    sessionEvent.name + ' - ' + speaker.userAccount.fullName
+  }
 }
