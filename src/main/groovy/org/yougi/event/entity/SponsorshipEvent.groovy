5b1c842b8357a7013210da78080b7ceee2155cc9
eprogramming <00hf11@gmail.com>
2015-04-26 00:31:38 -0300

    #27

--- src/main/groovy/org/yougi/event/entity/SponsorshipEvent.groovy
+++ src/main/groovy/org/yougi/event/entity/SponsorshipEvent.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,118 +18,44 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.event.entity;
+package org.yougi.event.entity
 
-import org.yougi.entity.Identified;
-import org.yougi.partnership.entity.Partner;
+import org.yougi.entity.Identified
+import org.yougi.partnership.entity.Partner
 
-import javax.persistence.*;
-import java.io.Serializable;
-import java.math.BigDecimal;
+import javax.persistence.*
+import java.io.Serializable
+import java.math.BigDecimal
 
 /**
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
 @Entity
-@Table(name = "sponsorship_event")
-public class SponsorshipEvent implements Serializable, Identified {
+@Table(name = 'sponsorship_event')
+class SponsorshipEvent implements Serializable, Identified {
 
-    private static final long serialVersionUID = 1L;
+  @Id
+  String id
 
-    @Id
-    private String id;
+  @ManyToOne
+  @JoinColumn(name = 'event', nullable = false)
+  Event event
 
-    @ManyToOne
-    @JoinColumn(name = "event", nullable = false)
-    private Event event;
+  @ManyToOne
+  @JoinColumn(name = 'partner', nullable = false)
+  Partner partner
 
-    @ManyToOne
-    @JoinColumn(name = "partner", nullable = false)
-    private Partner partner;
+  BigDecimal amount
 
-    private BigDecimal amount;
+  @Enumerated(EnumType.STRING)
+  @Column(name='sponsorship_level')
+  SponsorshipLevel sponsorshipLevel
 
-    @Enumerated(EnumType.STRING)
-    @Column(name="sponsorship_level")
-    private SponsorshipLevel sponsorshipLevel;
+  String description
 
-    private String description;
+  @Override
+  String toString() {
+    partner.name
+  }
 
-    @Override
-    public String getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Event getEvent() {
-        return event;
-    }
-
-    public void setEvent(Event event) {
-        this.event = event;
-    }
-
-    public Partner getPartner() {
-        return partner;
-    }
-
-    public void setPartner(Partner partner) {
-        this.partner = partner;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public SponsorshipLevel getSponsorshipLevel() {
-        return sponsorshipLevel;
-    }
-
-    public void setSponsorshipLevel(SponsorshipLevel sponsorshipLevel) {
-        this.sponsorshipLevel = sponsorshipLevel;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return this.partner.getName();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        SponsorshipEvent that = (SponsorshipEvent) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) {
-            return false;
-        }
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
-    }
 }
