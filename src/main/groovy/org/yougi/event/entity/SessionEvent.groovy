8f86ca3073e9496f6a4ad5dd2c8691835782f9e7
eprogramming <00hf11@gmail.com>
2015-04-26 00:06:56 -0300

    #27

--- src/main/groovy/org/yougi/event/entity/SessionEvent.groovy
+++ src/main/groovy/org/yougi/event/entity/SessionEvent.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,223 +18,70 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.event.entity;
+package org.yougi.event.entity
 
-import org.yougi.entity.Identified;
+import org.yougi.entity.Identified
 
-import javax.persistence.*;
-import java.io.Serializable;
-import java.util.Date;
-import java.util.List;
+import javax.persistence.*
+import java.io.Serializable
 
 /**
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
 @Entity
-@Table(name = "session")
-public class SessionEvent implements Serializable, Identified {
+@Table(name = 'session')
+class SessionEvent implements Serializable, Identified {
 
-    private static final long serialVersionUID = 1L;
+  @Id
+  String id
 
-    @Id
-    private String id;
+  @ManyToOne
+  @JoinColumn(name='event')
+  Event event
 
-    @ManyToOne
-    @JoinColumn(name="event")
-    private Event event;
+  String name
+  String description
 
-    private String name;
+  @Column(name='detailed_description')
+  String detailedDescription
 
-    private String description;
+  @Enumerated(EnumType.STRING)
+  @Column(name='experience_level')
+  ExperienceLevel experienceLevel
 
-    @Column(name="detailed_description")
-    private String detailedDescription;
+  String topics
 
-    @Enumerated(EnumType.STRING)
-    @Column(name="experience_level")
-    private ExperienceLevel experienceLevel;
+  @Column(name = 'start_date')
+  @Temporal(TemporalType.DATE)
+  Date startDate
 
-    private String topics;
+  @Column(name = 'end_date')
+  @Temporal(TemporalType.DATE)
+  Date endDate
 
-    @Column(name = "start_date")
-    @Temporal(TemporalType.DATE)
-    private Date startDate;
+  @Column(name = 'start_time')
+  @Temporal(TemporalType.TIME)
+  Date startTime
 
-    @Column(name = "end_date")
-    @Temporal(TemporalType.DATE)
-    private Date endDate;
+  @Column(name = 'end_time')
+  @Temporal(TemporalType.TIME)
+  Date endTime
 
-    @Column(name = "start_time")
-    @Temporal(TemporalType.TIME)
-    private Date startTime;
+  @ManyToOne
+  @JoinColumn(name='room')
+  Room room
 
-    @Column(name = "end_time")
-    @Temporal(TemporalType.TIME)
-    private Date endTime;
+  @ManyToOne
+  @JoinColumn(name='track')
+  Track track
 
-    @ManyToOne
-    @JoinColumn(name="room")
-    private Room room;
+  Boolean approved
 
-    @ManyToOne
-    @JoinColumn(name="track")
-    private Track track;
+  @Transient
+  List<Speaker> speakers
 
-    private Boolean approved;
-
-    @Transient
-    private List<Speaker> speakers;
-
-    public SessionEvent() {
-    }
-
-    public SessionEvent(String id) {
-        this.id = id;
-    }
-
-    @Override
-    public String getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Event getEvent() {
-        return event;
-    }
-
-    public void setEvent(Event event) {
-        this.event = event;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getDetailedDescription() {
-        return detailedDescription;
-    }
-
-    public void setDetailedDescription(String detailedDescription) {
-        this.detailedDescription = detailedDescription;
-    }
-
-    public ExperienceLevel getExperienceLevel() {
-        return experienceLevel;
-    }
-
-    public void setExperienceLevel(ExperienceLevel experienceLevel) {
-        this.experienceLevel = experienceLevel;
-    }
-
-    public String getTopics() {
-        return topics;
-    }
-
-    public void setTopics(String topics) {
-        this.topics = topics;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return this.endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    public Date getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(Date startTime) {
-        this.startTime = startTime;
-    }
-
-    public Date getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(Date endTime) {
-        this.endTime = endTime;
-    }
-
-    public Room getRoom() {
-        return room;
-    }
-
-    public void setRoom(Room room) {
-        this.room = room;
-    }
-
-    public Track getTrack() {
-        return track;
-    }
-
-    public void setTrack(Track track) {
-        this.track = track;
-    }
-
-    public Boolean getApproved() {
-        return approved;
-    }
-
-    public void setApproved(Boolean approved) {
-        this.approved = approved;
-    }
-
-    public List<Speaker> getSpeakers() {
-        return speakers;
-    }
-
-    public void setSpeakers(List<Speaker> speakers) {
-        this.speakers = speakers;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (!(object instanceof SessionEvent)) {
-            return false;
-        }
-        SessionEvent other = (SessionEvent) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return this.name;
-    }
+  @Override
+  String toString() {
+    name
+  }
 }
