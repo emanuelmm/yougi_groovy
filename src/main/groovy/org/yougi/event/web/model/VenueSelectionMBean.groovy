d0ea764c1f8972f46e5b2745922ad934f3b32d1a
eprogramming <00hf11@gmail.com>
2015-04-26 11:27:34 -0300

    #27

--- src/main/groovy/org/yougi/event/web/model/VenueSelectionMBean.groovy
+++ src/main/groovy/org/yougi/event/web/model/VenueSelectionMBean.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,17 +18,17 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.event.web.model;
+package org.yougi.event.web.model
 
-import org.yougi.event.business.EventVenueBean;
-import org.yougi.event.business.RoomBean;
-import org.yougi.event.business.VenueBean;
-import org.yougi.event.entity.Event;
-import org.yougi.event.entity.Room;
-import org.yougi.event.entity.Venue;
-import javax.ejb.EJB;
-import javax.faces.view.ViewScoped;
-import javax.inject.Named;
+import org.yougi.event.business.EventVenueBean
+import org.yougi.event.business.RoomBean
+import org.yougi.event.business.VenueBean
+import org.yougi.event.entity.Event
+import org.yougi.event.entity.Room
+import org.yougi.event.entity.Venue
+import javax.ejb.EJB
+import javax.faces.view.ViewScoped
+import javax.inject.Named
 
 /**
  * This class is used to manage the selection of the venue and room of an event
@@ -38,77 +38,54 @@ import javax.inject.Named;
  */
 @Named
 @ViewScoped
-public class VenueSelectionMBean implements Serializable {
+class VenueSelectionMBean implements Serializable {
 
-    @EJB
-    private VenueBean venueBean;
+  @EJB
+  private VenueBean venueBean
+  @EJB
+  private EventVenueBean eventVenueBean
+  @EJB
+  private RoomBean roomBean
 
-    @EJB
-    private EventVenueBean eventVenueBean;
+  List<Venue> venues
+  List<Room> rooms
+  String selectedVenue
+  String selectedRoom
+  String selectedEvent
 
-    @EJB
-    private RoomBean roomBean;
+  List<Venue> getVenues() {
+    Event event = new Event(selectedEvent)
+    venues = eventVenueBean.findEventVenues(event)
+    venues
+  }
 
-    private List<Venue> venues;
-
-    private List<Room> rooms;
-
-    private String selectedVenue;
-
-    private String selectedRoom;
-
-    private String selectedEvent;
-
-    public void setSelectedEvent(String selectedEvent) {
-        this.selectedEvent = selectedEvent;
+  List<Room> getRooms() {
+    if (selectedVenue) {
+      Venue venue = new Venue(selectedVenue)
+      rooms = roomBean.findRooms(venue)
+    } else {
+      rooms = null
     }
+    rooms
+  }
 
-    public List<Venue> getVenues() {
-        Event event = new Event(this.selectedEvent);
-        this.venues = eventVenueBean.findEventVenues(event);
-        return this.venues;
+  Venue getVenue() {
+    if (selectedVenue) {
+      return venueBean.find(selectedVenue)
     }
+    null
+  }
 
-    public List<Room> getRooms() {
-        if (this.selectedVenue != null) {
-            Venue venue = new Venue(selectedVenue);
-            this.rooms = roomBean.findRooms(venue);
-        } else {
-            this.rooms = null;
-        }
-        return this.rooms;
+  Room getRoom() {
+    if (selectedRoom) {
+      return roomBean.find(selectedRoom)
     }
+    null
+  }
 
-    public Venue getVenue() {
-        if (this.selectedVenue != null) {
-            return venueBean.find(selectedVenue);
-        } else {
-            return null;
-        }
-    }
+  void setSelectedVenue(String selectedVenue) {
+    this.selectedVenue = selectedVenue
+    selectedRoom = null
+  }
 
-    public Room getRoom() {
-        if (this.selectedRoom != null) {
-            return roomBean.find(selectedRoom);
-        } else {
-            return null;
-        }
-    }
-
-    public String getSelectedVenue() {
-        return selectedVenue;
-    }
-
-    public void setSelectedVenue(String selectedVenue) {
-        this.selectedVenue = selectedVenue;
-        this.selectedRoom = null;
-    }
-
-    public String getSelectedRoom() {
-        return selectedRoom;
-    }
-
-    public void setSelectedRoom(String selectedRoom) {
-        this.selectedRoom = selectedRoom;
-    }
 }
