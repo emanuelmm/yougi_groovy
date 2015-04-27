ea4bc424328754717f972adf9297cc08e9e143da
eprogramming <00hf11@gmail.com>
2015-04-26 23:18:33 -0300

    #27

--- src/main/groovy/org/yougi/reference/StorageDuration.groovy
+++ src/main/groovy/org/yougi/reference/StorageDuration.groovy
@@ -3,13 +3,13 @@
  * constantly sharing information and participating in social and educational
  * events. Copyright (C) 2011 Hildeberto Mendonça.
  *
- * This application is free software; you can redistribute it and/or modify it
+ * This application is free software you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation; either version 2.1 of the License, or (at your
+ * Free Software Foundation either version 2.1 of the License, or (at your
  * option) any later version.
  *
  * This application is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
  * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
  * License for more details.
  *
@@ -18,49 +18,28 @@
  * find it, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA.
  * */
-package org.yougi.reference;
+package org.yougi.reference
 
 /**
  * @author Hildeberto Mendonca - http://www.hildeberto.com
  */
-public enum StorageDuration {
+enum StorageDuration {
 
-    /**
-     * Less than one day.
-     */
-    TEMPORARY(0),
-    /**
-     * One day.
-     */
-    ONE_DAY(1),
-    /**
-     * 7 days.
-     */
-    ONE_WEEK(7),
-    /**
-     * 30 days.
-     */
-    ONE_MONTH(30),
-    /**
-     * 366 days.
-     */
-    ONE_YEAR(366),
-    /**
-     * 732 days.
-     */
-    TWO_YEARS(732),
-    /**
-     * 1098 days.
-     */
-    THREE_YEARS(1098);
+  TEMPORARY(0),
+  ONE_DAY(1),
+  ONE_WEEK(7),
+  ONE_MONTH(30),
+  ONE_YEAR(366),
+  TWO_YEARS(732),
+  THREE_YEARS(1098)
 
-    private Integer days;
+  private Integer days
 
-    StorageDuration(Integer days) {
-        this.days = days;
-    }
+  StorageDuration(Integer days) {
+    this.days = days
+  }
 
-    public Integer getDays() {
-        return this.days;
-    }
+  Integer getDays() {
+    days
+  }
 }
